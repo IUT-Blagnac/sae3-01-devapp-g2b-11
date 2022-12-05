4fca64d808c6763864eaccfa6ac2c4704ff2cf57
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-12-05 09:00:55 +0100

    réorganisation de l'orborescence des fichiers

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Royal Bio - Les roi du Bio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <header>
+        <?php include("src/pages/header/header.php"); ?>
+    </header>    
+
+    <main>
+
+    </main>
+    
+    <footer>
+        <?php include("src/pages/footer/footer.php"); ?>
+    </footer>
+</body>
+</html>
