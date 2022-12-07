824bcca08a633e451fdbf18a524152ab00c1409b
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-12-07 16:31:08 +0100

    Ajout de la charte graphique

--- index.php
+++ index.php
@@ -4,21 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Royal Bio - Les roi du Bio</title>
+    <title>E-commerce</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-
-    <header>
-        <?php include("src/pages/header/header.php"); ?>
-    </header>    
-
+    <?php include("src/pages/header/header.php") ?>
     <main>
         
     </main>
+    <?php include("src/pages/footer/footer.php") ?>
     
-    <footer>
-        <?php include("src/pages/footer/footer.php"); ?>
-    </footer>
 </body>
 </html>
---
+++ src/pages/header/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="src/pages/header/header.css">
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="navlogo">
+                <div class="logo"></div>
+                <div class="title">Royal Bio.</div>
+            </div>
+            <div class="navbar">
+                <ul>
+                    <div class="search-bar">
+                        <div class="icon"><i class="fa-sharp fa-solid fa-magnifying-glass"></i></div>
+                        <div class="input"><input type="text"></div>
+                    </div>
+                    
+                    <li><a href="#" class="button">Contact</a></li>
+                    <li><a href="#" class="button">Connexion</a></li>
+                    <li><a href="#" class="panier">Panier</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="subheader">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Alimentation</a><i class="fa-sharp fa-solid fa-sort-down"></i></li>
+                <li><a href="#">Hygiène</a><i class="fa-sharp fa-solid fa-sort-down"></i></li>
+                <li><a href="#">Maison</a><i class="fa-sharp fa-solid fa-sort-down"></i></li>
+            </ul>
+        </div>
+    </header>
+    
+</body>
+</html>
