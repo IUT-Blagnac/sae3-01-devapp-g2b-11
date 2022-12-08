509212bc8618ee04a85aae52079a1af043361f5c
Cédric-Alexandre Pascal <cedricalexandrepascal@gmail.com>
2022-12-08 11:30:17 +0100

    Mise en place d'un fichier de config

--- app/python/get_donnee_entrepot.py
+++ app/python/get_donnee_entrepot.py
@@ -4,28 +4,34 @@ import paho.mqtt.client as mqtt
 
 from os import *
 
+c = open('config.json', O_RDONLY)
+config = json.loads(read(c, 1000))
+
 mqttserver = "chirpstack.iut-blagnac.fr"
 mqttport = 1883
 
 def get_data(mqtt, obj, msg):
-    f = open(str("data.txt"), O_WRONLY | O_CREAT | O_TRUNC, 0o600)
+    f = open(config["nomFichier"], O_WRONLY | O_CREAT | O_TRUNC, 0o600)
     jsonMsg = json.loads(msg.payload)
-    donnee = jsonMsg["object"]["co2"]
-    msg = ("CO2:" + str(donnee)).encode()
-    write(f, bytes(msg + b"\n"))
-    print("CO2: ", donnee)
-    donnee2 = jsonMsg["object"]["temperature"]
-    msg2 = ("Temperature:" + str(donnee2)).encode()
-    write(f, bytes(msg2 + b"\n"))
-    print("Temérature:",donnee2)
-    if (donnee2 > 8):
-        print("ALERTE ! Température de l'entrepôt trop élevée pour la conservation des aliments !")
-    donnee3 = jsonMsg["object"]["humidity"]
-    msg3 = ("Humidité:" + str(donnee3)).encode()
-    write(f, bytes(msg3 + b"\n"))
-    print("Humidité : ",donnee3 )
-    if(donnee3 > 60):
-        print("ALERTE ! Humidité de l'entrepôt trop élevée pour la conservation des aliments !")
+    for data in config["data"]:
+        donnee = jsonMsg["object"][data]
+        if(data == "co2"):
+            msg = ("CO2:" + str(donnee)).encode()
+            write(f, bytes(msg + b"\n"))
+            print("CO2: ", donnee)
+        elif(data == "temperature"):
+            msg = ("Temperature:" + str(donnee)).encode()
+            if (donnee > config["seuiltemp"]):
+                print("ALERTE ! Température de l'entrepôt trop élevée pour la conservation des aliments !")
+            write(f, bytes(msg + b"\n"))
+            print("Temperature: ", donnee)
+        elif(data == "humidity"):
+            msg = ("Humidité:" + str(donnee)).encode()
+            if (donnee > config["seuilhum"]):
+                print("ALERTE ! Humidité de l'entrepôt trop élevée pour la conservation des aliments !")
+            write(f, bytes(msg + b"\n"))
+            print("Humidité: ", donnee)
+
     print("---------------------------------")
 
 print("Connexion au server MQTT...")
@@ -35,7 +41,6 @@ client.connect(mqttserver, mqttport, 600)
 
 client.subscribe("application/1/device/+/event/up")
 #24e124128c017760
-
 
 client.on_message = get_data
 
