1ef3d66f385ac3cb2f88f01722e37dbd8d525b6b
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-12-15 10:32:41 +0100

    Ajout du script des insertions dans la BD

---
+++ assets/BD/ScriptInsert.sql
@@ -0,0 +1,80 @@
+-- CATEGORIE MERE
+INSERT INTO CATEGORIE (idcategorie, nomcategorie)
+VALUES('1', 'Alimentation');
+INSERT INTO CATEGORIE (idcategorie, nomcategorie)
+VALUES('2', 'Hygiène');
+INSERT INTO CATEGORIE (idcategorie, nomcategorie)
+VALUES('3', 'Maison');
+
+-- CATEGORIE FILLE
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('01', '1', 'Fruits');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('02', '1', 'Légumes');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('03', '1', 'Viandes');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('04', '1', 'Crémerie');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('05', '1', 'Sucré');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('06', '1', 'Salé');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('07', '2', 'Gel douches');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('08', '2', 'Shampoings');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('09', '2', 'Soins');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('10', '3', 'Vaisselle');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('11', '3', 'Décoration');
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('12', '3', 'Linge de maison');
+
+-- SOUS CATEGORIE FILLE
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('001', '01', 'Fraise');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('002', '02', 'Haricot vert');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('003', '03', 'Poulet');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('004', '04', 'Fromage');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('005', '05', 'Bonbons');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('006', '06', 'Chips');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('007', '07', 'Cadum');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('008', '08', 'Dop');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('009', '09', 'Axe');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('010', '10', 'Assiettes');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('011', '11', 'Rangements');
+
+INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
+VALUES('012', '12', 'Literie');
+
+-- ///////////////////////////////////////////////////////////////
+
+-- Produits
+
+
+
+
