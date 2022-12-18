418df2a6952a2b6cfc7d313f38b67452a8025e6e
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-12-18 13:19:32 +0100

    Ajout du compte rendu web semaine 50

--- assets/BD/ScriptInsert.sql
+++ assets/BD/ScriptInsert.sql
@@ -1,10 +1,3 @@
--- CATEGORIE 
-INSERT INTO CATEGORIE (idcategorie, nomcategorie)
-VALUES('1', 'Alimentation');
-INSERT INTO CATEGORIE (idcategorie, nomcategorie)
-VALUES('2', 'Hygiène');
-INSERT INTO CATEGORIE (idcategorie, nomcategorie)
-VALUES('3', 'Entretien');
 
 -- CATEGORIE FILLE
 INSERT INTO CATEGORIE (idcategorie, idcategoriemere ,nomcategorie)
